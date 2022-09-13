a75ec3d562cc93f5b96289ba01e7594d6e6eb10a
Sabrina <smg21022@gmail.com>
2022-09-12 23:29:24 -0600

    task 4

--- index.js
+++ index.js
@@ -230,11 +230,11 @@ Use getArtistByIndex to do the following:
 
 🌟 EXAMPLE: if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(array,number) {
+  return `the artist at index ${array[number].id} is ${array[number].name}`;
 }
 
-
+console.log(getArtistByIndex(artists,0));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use listOfNames to do the following: 
@@ -244,10 +244,17 @@ Use listOfNames to do the following:
 🌟 EXAMPLE of return: ["Amedeo Modigliani", "Vasiliy Kandinskiy", "Diego Rivera"....]
 */
 
-function listOfNames(/*Your Code Here*/) {
-  /*Your Code Here*/
+function listOfNames(array) {
+  const tell = array.slice();
+  for(let i = 0; i < tell.length; i++)
+  if (tell[i].id <= 21){
+console.log(tell[i].name);
+  }  
 }
 
+console.log(listOfNames(artists));
+
+console.log(listOfNames(artists));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
